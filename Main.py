5c7e536cf88e92aa14739e58fbd601a39c23a558
steven <sagallagher@mail.bradley.edu>
2017-11-24 14:30:26 -0600

    cleaned main

--- Main.py
+++ Main.py
@@ -8,10 +8,8 @@ if __name__ == '__main__':
     # create object to parse config file
     cp = ConfigParser()
     # parse the configuration file
-    if not cp.parse(sys.argv[1]): exit()
+    if not cp.parse(sys.argv[1]): exit() # if one was not provided, exit
     # send the configuration file to the tester so it can use the settings provided
     t = Tester(cp)
-    print cp.settings
-
     # run the tests
     t.run()
