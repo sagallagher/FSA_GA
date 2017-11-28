acf8450da59f729ef9002195768a5e8470de275c
sagallagher <sagallagher@mail.bradley.edu>
2017-11-28 15:25:23 -0600

    formatting

--- InitializePopulation/RandomInitializePopulation.py
+++ InitializePopulation/RandomInitializePopulation.py
@@ -1,3 +1,5 @@
+# fill a genotype with randomly generated chromosomes
+
 from random import randint
 from Genotype.Genotype import *
 from Genotype.Chromosome import *
@@ -20,11 +22,6 @@ class RandomInitializePopulation():
 
     # generate a random finite state machine
     def generateRandomFSM(self, min_states, max_states):
-
-        '''
-        BUG: currently generates NFSA. Might be easier if only generate DFSA
-        '''
-
         number_of_states = randint(min_states, max_states)
         fsm = []
         for number_state in xrange(number_of_states):
@@ -39,23 +36,29 @@ class RandomInitializePopulation():
     def generateRandomFinalStates(self, fsm):
         final_states = []
         number_of_final_states = randint(1,len(fsm))
+        
         for state in xrange(number_of_final_states):
             random_final_state = randint(0,len(fsm)-1)
+            
             if random_final_state not in final_states:
                 final_states.append(random_final_state)
+                
         return final_states
 
     # fill genotype with desired population size
     def fillGenotype(self, genotype):
-
         for i in xrange(self.population_size):
             fsm = self.generateRandomFSM(self.min_states, self.max_states)
+            
             final_states = self.generateRandomFinalStates(fsm)
+            
             genotype.pushChromosome(Chromosome(fsm, final_states))
 
     def introduceDiversity(self, genotype):
         fsm = self.generateRandomFSM(self.min_states, self.max_states)
+        
         final_states = self.generateRandomFinalStates(fsm)
+        
         genotype.pushChromosome(Chromosome(fsm, final_states))
 
 
