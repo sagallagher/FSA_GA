598531c2b978ea0a75c23c0a9888117fac0d58f8
sagallagher <sagallagher@mail.bradley.edu>
2017-11-28 12:59:20 -0600

    formatting

--- Crossover/RandomCrossover.py
+++ Crossover/RandomCrossover.py
@@ -10,13 +10,17 @@ class RandomCrossover():
     def getMostFit(self, genotype):
         most_fit = Chromosome([],[])
         most_fit2 = Chromosome([],[])
+        
+        # find the first most fit
         for chromosome in genotype.chromosomes:
             if chromosome.fitness > most_fit.fitness: most_fit = chromosome
 
+        # find the second most fit
         for chromosome in genotype.chromosomes:
             if (chromosome.fitness > most_fit2.fitness
                 and chromosome.fsm != most_fit.fsm): most_fit2 = chromosome
-
+                
+        # reutrn the two most fit chromosomes
         return most_fit, most_fit2
 
     # get the two least fit chromosomes
@@ -37,31 +41,32 @@ class RandomCrossover():
 
     # get the result of the cross over of the most fit parents
     def getOffspring(self, genotype):
-
+        # get the two most fit chromosomes
         (parent1, parent2) = self.getMostFit(genotype)
 
+        # create empty chromosomes to fill with child data
         child1 = Chromosome([],[])
         child2 = Chromosome([],[])
 
         # get a random pivot
         random_pivot = randint(0,len(parent1.fsm))
 
-
         # one point crossover on rows
         child1.fsm = parent1.fsm[:random_pivot:] + parent2.fsm[random_pivot:]
         child2.fsm = parent2.fsm[:random_pivot] + parent1.fsm[random_pivot:]
-        '''
-        crossover in such a way that the final states follow its associated rows
-        during the crossover
-        '''
 
-        # child1 gets tail of parent1 and head of parents2
+        #
+        # crossover the rows associated final states
+        #
+        
+        # crossover the first half of the final states
         for row in xrange(random_pivot):
             if row in parent1.final_states:
                 child1.final_states.append(row)
             if row in parent2.final_states:
                 child2.final_states.append(row)
-
+                
+        # crossover the second half of the final states
         for row in range(random_pivot,len(parent2.fsm)):
             if row in parent1.final_states:
                 child2.final_states.append(row)
@@ -69,25 +74,17 @@ class RandomCrossover():
                 child1.final_states.append(row)
 
 
-        # child2 gets tail of parent2 and head of parent1
-        '''
-        # ^Above method performs better
-        random_pivot2 = randint(0, len(parent2.final_states))
-        # one point crossover on final states
-        child1.final_states = parent1.final_states[:random_pivot2] + parent2.final_states[random_pivot2:]
-        child2.final_states = parent2.final_states[random_pivot2:] + parent2.final_states[:random_pivot2]
-        '''
-
     # replace the least fit chromosomes with the offspring of the most fit
     def selectSurvivors(self, child1, child2, genotype):
+        
+        # get 2 chromosomes with least fitness
         (least_fit1, least_fit2) = self.getLeastFit(genotype)
-
-
-
+        
+        # remove the least fit chromosomes
         genotype.chromosomes.remove(least_fit1)
-
         genotype.chromosomes.remove(least_fit2)
 
+        # replace them with the child chromosomes
         genotype.pushChromosome(child1)
         genotype.pushChromosome(child2)
 
