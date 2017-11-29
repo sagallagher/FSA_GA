f80ec928c07db7b6c4b9d2b54195bc791defd22d
me <sagallagher@mail.bradley.edu>
2017-11-28 19:37:53 -0600

    updated compare results

--- Test/CompareResults/CompareResults.py
+++ Test/CompareResults/CompareResults.py
@@ -1,7 +1,14 @@
 
-import os
+# run the script with a directoy of outputs as a parameter
+# script will display graphs of the number of solutions checked for each example
+# in each file
+# additionaly, misc stats are displayed
+
+import os, os.path
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+import sys
 
 # return the list of solution counts at the bottom of the output files
 def getSolutionCountList(filename):
@@ -16,21 +23,11 @@ def getSolutionCountList(filename):
     # parse that last line (which is the list) into a list
     return [int(x) for x in last_line.replace('[','').replace(']','').replace(' ','').split(',')]
 
-
-import math
-
-
-def plotOutputs(directory):
-
-    # used to store the best average
-    best = -1
-
-    # store the path to the best output file
-    result = ''
-
+# plot the outputs of each output file in a given directory
+def plotOutputs(directory, clear_ticks = False):
     # dimension for the canvas
     dim = int(math.ceil((math.sqrt(len(os.listdir(directory))))))
-    print 'dim',dim
+
     # canvas for all the plots
     f, canvas = plt.subplots(dim, dim)
 
@@ -42,24 +39,24 @@ def plotOutputs(directory):
         solution_list = getSolutionCountList(directory+'/'+outfile)
 
         # track next location of subplot
-        if i < dim-1:
-            i+=1
+        if i < dim-1: i+=1
         else:
             i = 0
             j+=1
 
         # add subplot to canvas
-        canvas[i,j].bar(np.arange(len(solution_list)),solution_list)
+        canvas[i,j].scatter(np.arange(len(solution_list)),solution_list)
         canvas[i,j].set_title(outfile)
 
-    # clear ticks in each sub plot
-    for i in range(0,dim):
-        for j in range(0,dim):
-            canvas[i,j].set_yticklabels([])
-            canvas[i,j].set_xticklabels([])
+    if clear_ticks:
+        # clear ticks in each sub plot
+        for i in range(0,dim):
+            for j in range(0,dim):
+                canvas[i,j].set_yticklabels([])
+                canvas[i,j].set_xticklabels([])
 
     # display the plot
-
+    plt.tight_layout()
     plt.show()
 
 # given a list of output files, return the one that contains the best average
@@ -73,16 +70,41 @@ def bestAverage(directory):
 
     # search through each file looking for
     for outfile in os.listdir(directory):
-        solution_list = getSolutionCountList(directory+'/'+outfile)
+        solution_list = getSolutionCountList(os.path.join(directory,outfile))
 
-        average = sum(solution_list)/float((len(solution_list)))
+        average = np.mean(solution_list)
 
-        if average < best or best == -1:
+        if (average < best or best == -1) and average >= 0:
             best = average
             result = str(outfile)
 
 
     return result
 
-print bestAverage('/home/me/scripts/python/AutoAutomata/FSA_GA/Data/output')
-plotOutputs('/home/me/scripts/python/AutoAutomata/FSA_GA/Data/output')
+def giveUpCount(solution_list):
+    return len([i for i in solution_list if i < 0])
+
+
+def getStats(file_path):
+    solution_list = getSolutionCountList(file_path)
+
+    return np.mean(solution_list),np.median(solution_list),giveUpCount(solution_list)
+
+def displayStats(directory):
+
+
+    for outfile in os.listdir(directory):
+        (mean, median, give_up) = getStats(os.path.join(directory,outfile))
+        print '--',outfile,'--'
+        print 'MEAN:%20s'% mean
+        print "MEDIAN:%20s" % median
+        print "GIVE UP:%20s" % give_up
+        print "BEST AVG:%20s" % bestAverage(directory)
+
+# plot the average solutions checked in each file
+def plotAverages(directory):
+    pass
+    
+
+displayStats(sys.argv[1])
+plotOutputs(sys.argv[1])
